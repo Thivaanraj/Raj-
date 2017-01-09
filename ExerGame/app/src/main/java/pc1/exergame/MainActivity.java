88bb58893b8aecb23a6653d16dce33c52cb7489a
Roman Karaba <roman.karaba.jr@gmail.com>
2017-01-09 21:13:47 +0100

    project push

---
+++ ExerGame/app/src/main/java/pc1/exergame/MainActivity.java
@@ -0,0 +1,13 @@
+package pc1.exergame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
