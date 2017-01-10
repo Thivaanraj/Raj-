88fc9cf4900cd734421ee310a409e7cd68549b73
Roman Karaba <roman.karaba.jr@gmail.com>
2017-01-10 12:12:52 +0100

    sign in activity layout

---
+++ ExerGame/app/src/main/java/pc1/exergame/SignInActivity.java
@@ -0,0 +1,13 @@
+package pc1.exergame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
