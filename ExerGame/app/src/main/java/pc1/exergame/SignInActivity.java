0e62d6b59bf506f75c901595b86c26db0bd88c39
Roman Karaba <roman.karaba.jr@gmail.com>
2017-01-10 15:26:36 +0100

    lets pray

--- ExerGame/app/src/main/java/pc1/exergame/SignInActivity.java
+++ ExerGame/app/src/main/java/pc1/exergame/SignInActivity.java
@@ -1,13 +1,68 @@
 package pc1.exergame;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText etPass;
+    private EditText etEmail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        etEmail = (EditText) findViewById(R.id.login);
+        etPass = (EditText) findViewById(R.id.password);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    System.out.println(user.getUid() + " signed in");
+                } else {
+                    System.out.println("currently signed out");
+                }
+            }
+        };
+
+        final Button loginButton = (Button) findViewById(R.id.loginbnt);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
 }
